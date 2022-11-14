fbc8928500a409b6966f4484a95b179afb706574
sanaebadi <sana@journeyhero.net>
2022-11-14 20:54:33 +0200

    CREATED A USER PROFILE LAYOUT

--- app/src/main/java/info/sanaebadi/rubikamp_3/MainActivity.java
+++ app/src/main/java/info/sanaebadi/rubikamp_3/MainActivity.java
@@ -1,11 +1,10 @@
 package info.sanaebadi.rubikamp_3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.user_profile_layout);
 
-        button= findViewById(R.id.button_test);
-
-      button.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Toast.makeText(MainActivity.this, "HEY RUBIKAMNPPPP!!", Toast.LENGTH_SHORT).show();
-          }
-      });
     }
+
+
 }
